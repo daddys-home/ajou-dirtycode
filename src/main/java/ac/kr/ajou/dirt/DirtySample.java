8e2c4f82840d6220696b3441949134140c767391
yeongin <pyi7628@gmail.com>
2019-07-23 13:04:33 +0900

    isName method로 이름 변경

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -9,12 +9,12 @@ class DirtySample {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!nameIs(items[i], "Sulfuras, Hand of Ragnaros")) {
+            if (!isName(items[i], "Sulfuras, Hand of Ragnaros")) {
                 if (isAgedBrieOrBackstagePasses(items[i])) {
                     isItemQualityLessThan50(i);
                 } else if (items[i].quality > 0) addIntegerToQuality(i, -1);
 
-                items[i].sellIn -= 1;
+                items[i].sellIn--;
                 isItemSellInNegative(i);
             }
         }
@@ -23,7 +23,7 @@ class DirtySample {
     private void isItemQualityLessThan50(int index) {
         if (items[index].quality < 50) {
             addIntegerToQuality(index,1);
-            if (nameIs(items[index], "Backstage passes to a TAFKAL80ETC concert")
+            if (isName(items[index], "Backstage passes to a TAFKAL80ETC concert")
                     && items[index].sellIn < 11 && items[index].quality < 50) {
                 addIntegerToQuality(index, 1);
                 if (items[index].sellIn < 6 && items[index].quality < 50)
@@ -35,9 +35,9 @@ class DirtySample {
 
     public void isItemSellInNegative(int index) {
         if (items[index].sellIn < 0) {
-            if (nameIs(items[index], "Aged Brie") && items[index].quality < 50) {
+            if (isName(items[index], "Aged Brie") && items[index].quality < 50) {
                 addIntegerToQuality(index,1);
-            } else if (nameIs(items[index], "Backstage passes to a TAFKAL80ETC concert"))
+            } else if (isName(items[index], "Backstage passes to a TAFKAL80ETC concert"))
                 items[index].quality = 0;
             else if (!isAgedBrieOrBackstagePasses(items[index]) && items[index].quality > 0)
                 addIntegerToQuality(index, -1);
@@ -48,13 +48,13 @@ class DirtySample {
         items[index].quality += integerToAdd;
     }
 
-    public boolean nameIs(Item item, String s) {
+    public boolean isName(Item item, String s) {
         return item.name.equals(s);
     }
 
     public boolean isAgedBrieOrBackstagePasses(Item item) {
-        return nameIs(item, "Aged Brie")
-                || nameIs(item, "Backstage passes to a TAFKAL80ETC concert");
+        return isName(item, "Aged Brie")
+                || isName(item, "Backstage passes to a TAFKAL80ETC concert");
     }
 
 
